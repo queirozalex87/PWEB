c3af39e46627832f9ce891d34504b150260c5e81
queirozalex87 <33673607+queirozalex87@users.noreply.github.com>
2020-10-14 22:17:36 -0300

    Add files via upload

---
+++ Atividade8/Aluno.js
@@ -0,0 +1,15 @@
+var aluno1=new Object();
+aluno.nome="Alex";
+aluno.ra=0030481723038;
+alert("aluno"+aluno1);
+
+var aluno2={};
+aluno.nome="Alex";
+aluno.ra=0030481723038;
+alert("aluno"+aluno2);
+
+var aluno3={
+nome:"Alex",
+ra:0030481723038
+};
+alert("aluno"+aluno3);
